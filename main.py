d902c4619c6bbd01b62373086d1183729b361085
Lucas Gorrie <n11277211@qut.edu.au>
2022-10-11 20:42:53 +1000

    added comments

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from website import create_app, db
 import website.models
 
 if __name__=='__main__':
+    # Creates application environment
     app=create_app()
     app.run(debug=True)
+
+
+    # Initialises DB with structure
     ctx=app.app_context()
     ctx.push()
     db.create_all()
