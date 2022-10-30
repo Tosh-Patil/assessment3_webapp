ac4f1d3a91c825b547f9d64066a0ed50ed5fffde
Tosh Patil <toshith123@gmail.com>
2022-10-30 16:53:57 +1000

    Added blueprint views with some backend variables and added eventName to Event sql model

--- website/views.py
+++ website/views.py
@@ -9,18 +9,33 @@ bp = Blueprint('main', __name__, template_folder='templates')
 def index():
     return render_template('index.html')
 
-@bp.route('/event-info')
-def event_info():
+@bp.route('/event-info', methods=['GET', 'POST'])
+def event_info(event_id):
+
+    event = Event.query.get(event_id)
+    event_name = event.eventName
+    event_info = event.description
+    event_date = event.eventDate
+    event_ticket_types = event.ticketTypes
+    event_status = event.eventStatus
+
+    comments = Comment.query.filter_by(eventId = event_id).all()
+
+    if request.method == 'POST':
+        #Booking tickets backend here
 
     return render_template('Event_Info_Page.html')
 
-@bp.route('/event-creation')
+@bp.route('/event-creation', methods=['GET', 'POST'])
 def event_creation():
+
+    if request.method == 'POST':
+        #creating an event backend here
 
     return render_template('Event_Creation')
 
 @bp.route('Booking_History.html')
-def booking_histor():
+def booking_history():
 
     return render_template('Booking_History')
 
