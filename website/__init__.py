285cdc1f7b21efe80166b71a2612fb5d1f9d63e4
Rileyjake78 <115766176+Rileyjake78@users.noreply.github.com>
2022-11-02 13:49:22 +1000

    Update _init_.py error handling
    Added two error handling messages, imported render_template. Created variable app=Flask(__name__) to allow error handlers to register app variable.

--- website/__init__.py
+++ website/__init__.py
@@ -1,10 +1,11 @@
 #import flask - from the package import class
-from flask import Flask 
+from flask import Flask, render_template 
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 db=SQLAlchemy()
+app=Flask(__name__)
 
 #create a function that creates a web application
 # a web server will run this web application
@@ -47,5 +48,14 @@ def create_app():
     
     return app
 
+@app.errorhandler(404) 
+# inbuilt function which takes error as parameter 
+def not_found(e): 
+  return render_template("404.html")
+
+@app.errorhandler(500) 
+# inbuilt function which takes error as parameter 
+def not_found(e):
+  return render_template("500.html")
 
 
